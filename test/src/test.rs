47377eb147a0df87fce63c519a3f61412a46ee0a
DarumaDocker <DarumaDocker@gmail.com>
2022-10-10 10:52:08 +0800

    test https req

--- test/src/test.rs
+++ test/src/test.rs
@@ -1,12 +1,14 @@
+use http_req::request;
 #[allow(unused_imports)]
 use wasmedge_bindgen::*;
 use wasmedge_bindgen_macro::*;
 
 #[wasmedge_bindgen]
 pub fn run(user: String) -> String {
-    serde_json::json!({
-        "text": "welcome",
-        "user": user,
-    })
-    .to_string()
+    let mut writer = Vec::new(); //container for body of a response
+    let res = request::get("https://httpbin.org/get", &mut writer).unwrap();
+
+    println!("Status: {} {}", res.status_code(), res.reason());
+    println!("Headers {}", res.headers());
+    String::from_utf8_lossy(&writer).to_string()
 }
