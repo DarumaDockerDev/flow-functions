4a232e345d02819fbd0c30e6df086c00d2e48eab
DarumaDocker <DarumaDocker@gmail.com>
2022-10-26 11:14:03 +0800

    update telegram ping

--- telegram/telegram/ping/javascript/index.js
+++ telegram/telegram/ping/javascript/index.js
@@ -3,7 +3,7 @@
     if (payload["message"]["text"] === "/ping")
         return JSON.stringify({
             chat_id: payload["message"]["chat"]["id"],
-            text: "__PONG\\!__",
+            text: "__PONG\\!PONG\\!__",
             parse_mode: "MarkdownV2",
             reply_to_message_id: payload["message"]["message_id"]
         });
